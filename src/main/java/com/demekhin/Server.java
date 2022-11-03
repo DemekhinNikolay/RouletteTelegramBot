cab4b4f678af6516e8b3209d3c57944aef82e890
N <Nescafe.gold.1988@mail.ru>
2022-11-03 18:17:53 +0300

    TestCommit Structure

---
+++ src/main/java/com/demekhin/Server.java
@@ -0,0 +1,4 @@
+package com.demekhin;
+
+public class Server {
+}
---
+++ src/main/java/com/demekhin/TelegramBot.java
@@ -0,0 +1,22 @@
+package com.demekhin;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+
+    }
+}
