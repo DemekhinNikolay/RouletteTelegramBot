8dd506925d3d305114f9a61e8e546eff13b7c117
N <Nescafe.gold.1988@mail.ru>
2022-11-04 00:52:45 +0300

    full processing of the black bet

--- src/main/java/com/demekhin/Server.java
+++ src/main/java/com/demekhin/Server.java
@@ -3,8 +3,28 @@ package com.demekhin;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Server {
+
+
+
+
+
+
+
+    private final List<String> colorArray = IntStream.range(0, 37).boxed()
+            .map(x -> {
+                if (x == 0) {
+                    return x + " green";
+                }
+                if (x % 2 == 0) {
+                    return x + " red";
+                } else {
+                    return x + " black";
+                }
+            }).toList();
+
     public void addButton(List<InlineKeyboardButton> buttonList, String setText , String callback) {
 
         InlineKeyboardButton button = new InlineKeyboardButton();
--- src/main/java/com/demekhin/TelegramBot.java
+++ src/main/java/com/demekhin/TelegramBot.java
@@ -10,6 +10,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
@@ -21,6 +22,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     public String getBotToken() {
         return "5349494181:AAG_p38yQGfPdvbzGP9IrIyvmjS0W9FMSeA";
     }
+
 
     @Override
     public void onUpdateReceived(Update update) {
@@ -42,52 +44,44 @@ public class TelegramBot extends TelegramLongPollingBot {
             long chatId = update.getCallbackQuery().getMessage().getChatId();
 
             if (callbackData.equals("betBlack")) {
+
                 String text = "вы выбрали ставку на черное";
                 executeEditMessageText(text, chatId, messageId);
+                amountSelectionKeyboard(chatId);
             }
             else if (callbackData.equals("betRed")) {
                 String text = "вы выбрали ставку на красное";
+                executeEditMessageText(text, chatId, messageId);
+                amountSelectionKeyboard(chatId);
+            }
+
+            else if (callbackData.equals("bet5")) {
+                betAmount = 5;
+                resultRandomNumber();
+                if ((resultRandom & 1) == 0) {
+                    balance -= betAmount;
+                    victoryOrDefeat = "Проигрыш";
+                } else {
+                    balance += betAmount * 2;
+                    victoryOrDefeat = "Победа";
+                }
+                String text = "вы выбрали сумму ставки " + betAmount + " $" + " у вас выпало " + resultRandomColor + " Это: " + victoryOrDefeat +
+                        " Ваш баланс " + balance;
                 executeEditMessageText(text, chatId, messageId);
             }
         }
 
     }
 
+    private int balance = 1000;
+    private int betAmount;
+    private String victoryOrDefeat;
+    private int resultRandom;
+    private String resultRandomColor;
+
+
     private void startCommandReceived(long chatId) {
-
-        Server rouletteServer = new Server();
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText("Hi, choose the type of bet\n" +
-                "1 - black\n" +
-                "2 - red\n" +
-                "3 - Number\n" +
-                "4 - Even\n" +
-                "5 - Odd\n" +
-                "6 - From 1 to 18\n" +
-                "7 - From 19 to 36");
-
-        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
-        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
-
-        rouletteServer.addButton(rowInLine, "1", "betBlack");
-        rouletteServer.addButton(rowInLine, "2","betRed");
-        rouletteServer.addButton(rowInLine, "3","betNumber");
-        rouletteServer.addButton(rowInLine, "4","betEven");
-        rouletteServer.addButton(rowInLine, "5","betOdd");
-        rouletteServer.addButton(rowInLine, "6","bet1to18");
-        rouletteServer.addButton(rowInLine, "7","bet19to36");
-
-        rowsInLine.add(rowInLine);
-        markupInline.setKeyboard(rowsInLine);
-        message.setReplyMarkup(markupInline);
-
-        try {
-            execute(message);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
-        }
+        betSelectionKeyboard(chatId);
     }
 
     private void stopCommandReceived(long chatId, String name) {
@@ -120,6 +114,86 @@ public class TelegramBot extends TelegramLongPollingBot {
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private void betSelectionKeyboard (long chatId) {
+        Server rouletteServer = new Server();
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Hi, choose the type of bet\n" +
+                "1 - black\n" +
+                "2 - red\n" +
+                "3 - Number\n" +
+                "4 - Even\n" +
+                "5 - Odd\n" +
+                "6 - From 1 to 18\n" +
+                "7 - From 19 to 36");
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+
+        rouletteServer.addButton(rowInLine, "1", "betBlack");
+        rouletteServer.addButton(rowInLine, "2","betRed");
+        rouletteServer.addButton(rowInLine, "3","betNumber");
+        rouletteServer.addButton(rowInLine, "4","betEven");
+        rouletteServer.addButton(rowInLine, "5","betOdd");
+        rouletteServer.addButton(rowInLine, "6","bet1to18");
+        rouletteServer.addButton(rowInLine, "7","bet19to36");
+
+        rowsInLine.add(rowInLine);
+        markupInline.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInline);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void amountSelectionKeyboard (long chatId) {
+        Server rouletteServer = new Server();
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выберите сумму ставки ваш банк $" + balance);
+
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+
+        rouletteServer.addButton(rowInLine, "5", "bet5");
+        rouletteServer.addButton(rowInLine, "10","bet10");
+        rouletteServer.addButton(rowInLine, "25","bet25");
+        rouletteServer.addButton(rowInLine, "50","bet50");
+
+        rowsInLine.add(rowInLine);
+        markupInline.setKeyboard(rowsInLine);
+        message.setReplyMarkup(markupInline);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void resultRandomNumber () {
+        final List<String> colorArray = IntStream.range(0, 37).boxed()
+                .map(x -> {
+                    if (x == 0) {
+                        return x + " green";
+                    }
+                    if (x % 2 == 0) {
+                        return x + " red";
+                    } else {
+                        return x + " black";
+                    }
+                }).toList();
+
+        resultRandom = (int) Math.floor(Math.random() * colorArray.size());
+        resultRandomColor = colorArray.get(resultRandom);
+
     }
 
 }
